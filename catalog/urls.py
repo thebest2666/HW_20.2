57e51c37a782077683c12071495ffcc87af95a3a
alexander anciferov <sania.antsiferoff@yandex.ru>
2024-10-01 18:05:18 +0800

    апгрейд каталога

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import product, contacts, product_detail
+from catalog.views import CatalogListView, ProductDetailView, ContactTemplateView
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path("", product, name="product"),
-    path("contacts/", contacts, name="contacts"),
-    path("product/<int:pk>/", product_detail, name="product_detail"),
+    path("", CatalogListView.as_view(), name="product"),
+    path("contacts/", ContactTemplateView.as_view(), name="contacts"),
+    path("product/<int:pk>/", ProductDetailView.as_view(), name="product_detail"),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,24 +1,25 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView, TemplateView
 
 from catalog.models import Product
 
 
-def product(request):
-    products = Product.objects.all()
-    context = {"products": products}
-    return render(request, "products.html", context)
+class CatalogListView(ListView):
+    model = Product
 
 
-def contacts(request):
-    if request.method == "POST":
-        name = request.POST.get("name")
-        phone = request.POST.get("phone")
-        message = request.POST.get("message")
-        print(f"{name} ({phone}): {message}")
-    return render(request, "contacts.html")
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = "catalog/catalog_detail.html"
 
 
-def product_detail(request, pk):
-    product = get_object_or_404(Product, pk=pk)
-    context = {"product": product}
-    return render(request, "product_detail.html", context)
+class ContactTemplateView(TemplateView):
+    template_name = "contacts.html"
+
+    def post(self, request):
+        if request.method == "POST":
+            name = request.POST.get("name")
+            phone = request.POST.get("phone")
+            message = request.POST.get("message")
+            print(f"{name} {phone} {message}")
+        return render(request, "contacts.html")
